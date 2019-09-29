9b201b784a83d87f71067e979437092a570305b3
Jesse Kawell <jakawell@gmail.com>
2019-09-29 00:03:21 -0400

    Add support for importing saved file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,23 @@
+import { PokemonSpecies } from './pokemonSpecies';
+import { Move } from './move';
+
 export { PokemonSpecies } from './pokemonSpecies';
 export { Move } from './move';
 export { Pokemon } from './pokemon';
+
+export class GameMasterImportResult {
+  constructor(
+    public speciesList: Map<string, PokemonSpecies>,
+    public movesList: Map<string, Move>,
+  ) {}
+}
+
+/**
+ * Imports the data from a previously imported game master that was saved as JSON.
+ * @param jsonObject The JSON object to import.
+ */
+export const ImportFromSaved = (jsonObject: any): GameMasterImportResult => new GameMasterImportResult(
+  new Map(Object.entries(jsonObject.species)
+    .map(([key, value]) => [key, PokemonSpecies.fromParsed(value as PokemonSpecies)])),
+  new Map(Object.entries(jsonObject.moves)),
+);
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -7,6 +7,8 @@ import {
   Move,
   Pokemon,
   PokemonSpecies,
+  GameMasterImportResult,
+  ImportFromSaved,
 } from '../src';
 import fakeGameMaster from './mockData/mockGameMaster.json';
 
@@ -20,4 +22,45 @@ test('should export all interfaces and models', () => {
   expect(new Pokemon(
     PokemonSpecies.fromRawMaster((fakeGameMaster.itemTemplates[0] as unknown as IPokemonTemplate), 3, 'Venusaur'),
     1, 1, 1, 1)).toBeDefined();
+  expect(new GameMasterImportResult(new Map(), new Map())).toBeDefined();
+});
+
+test('should import data from parsed game master', () => {
+  const imported = ImportFromSaved({
+    species: {
+      VENUSAUR_NORMAL: {
+        pokedexNumber: 3,
+        speciesId: 'VENUSAUR',
+        form: 'NORMAL',
+        speciesName: 'Venusaur',
+        types: ['GRASS', 'POISON'],
+        fastMoves: ['RAZOR_LEAF_FAST', 'VINE_WHIP_FAST'],
+        chargeMoves: ['SLUDGE_BOMB', 'PETAL_BLIZZARD', 'SOLAR_BEAM', 'RETURN', 'FRUSTRATION'],
+        baseAttack: 198,
+        baseDefense: 189,
+        baseStamina: 190,
+        isFormless: false,
+      },
+    },
+    moves: {
+      WRAP: {
+        id: 'WRAP',
+        type: 'NORMAL',
+        pveStats: {
+          power: 60,
+          energyDelta: -33,
+          castTime: 2900,
+        },
+        pvpStats: {
+          power: 60,
+          energyDelta: -45,
+          turns: 1,
+        },
+      },
+    },
+  });
+  expect(imported.speciesList.size).toBe(1);
+  expect((imported.speciesList.get('VENUSAUR_NORMAL') as PokemonSpecies).speciesId).toBe('VENUSAUR');
+  expect(imported.movesList.size).toBe(1);
+  expect((imported.movesList.get('WRAP') as Move).pvpStats.energyDelta).toBe(-45);
 });
